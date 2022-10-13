657d945b663e0ca5e588e90cade07258e22c9c51
Angel Marte <robotking134@gmail.com>
2022-10-13 15:23:08 -0400

    commit tree 82e5f9da69efc2f2555c19704865888c48677fed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const pSelect = document.querySelector('p')
+document.addEventListener("DOMContentLoaded", function(){
+    pSelect.textContent = "This is really cool!";
+    
+});
+
